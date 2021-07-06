088e599a1fcd31b99dc06eec1bef30ffa22a2f99
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-07-05 18:55:12 -0700

    css fixes new posting fix

--- controllers/dashboardRoute.js
+++ controllers/dashboardRoute.js
@@ -5,14 +5,13 @@ const { User, Post } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", withAuth, async (req, res) => {
-  console.log("hit", req.body);
   try {
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ["password"] },
       include: [{ model: Post }],
     });
     const user = userData.get({ plain: true });
-
+    console.log("hit", user);
     res.render("dashboard", {
       ...user,
       logged_in: req.session.logged_in,
