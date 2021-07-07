9681d391e5cdf1ae721558f3bb4093b6d809d806
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-07-06 18:15:49 -0700

    minor login fixes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,15 +3,12 @@ const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
-  console.log(req.body);
-  console.log(req.session);
   try {
     const newComment = await Comment.create({
       comment: req.body.commentary,
       user_id: req.session.user_id,
       post_id: req.body.post_id,
     });
-
     res.redirect(req.header("Referrer"));
   } catch (error) {
     res.status(500).json(error);
